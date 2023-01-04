d100a036cba4a2956406ccaf4cb79967907cd3af
Whelisson Dias <whelidias@hotmail.com>
2023-01-04 07:12:32 -0300

    create routes express

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { response } = require('express');
+const express = require('express');
+
+const app = express();
+
+app.get('/courses', (req, res) => {
+    return res.json(["Curso 1","Curso 2","Curso 3"])    
+});
+
+app.post('/courses', (req, res) => {
+    return res.json(["Curso 1","Curso 2","Curso 3","Curso 4"]) 
+});
+
+app.put('/courses/:id', (req, res) => {
+    return res.json(["Curso 6","Curso 2","Curso 3","Curso 4"]) 
+});
+
+app.patch('/courses/:id', (req, res) => {
+    return res.json(["Curso 6","Curso 10","Curso 3","Curso 4"]) 
+});
+
+app.delete('/courses/:id', (req, res) => {
+    return res.json(["Curso 3","Curso 4"]) 
+})
+
+app.listen(3333);
